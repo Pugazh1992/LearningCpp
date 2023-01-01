6c995d18dfbb40ffaee013046b006b08013e3de7
Pugazhenthi Sivasankar <76005760+Pugazh1992@users.noreply.github.com>
2023-01-01 15:24:02 -0500

    Add files via upload

---
+++ detRepWords.cpp
@@ -0,0 +1,23 @@
+// This program detects repeated words.
+#include "std_lib_facilities.h"
+
+//int main()
+//{
+//	int number_of_words = 0;	
+//	string previous = " "; // previous word; initialized to “not a word”
+//	string current; // current word
+//	while (cin>>current) { // read a stream of words
+//		++number_of_words;
+////  		cout << "The current word is " << current << '\n';
+//  		if (previous == current) // check if the word is the same as last
+//     	   cout << "word number " << number_of_words
+//			<< " repeated: " << current << '\n';
+//  		previous = current;
+//}
+//}
+
+int main()
+{
+string s = "Goodbye, cruel world! ";
+cout << s << '\n';
+}
